4130c5c7f7d3ce59cb9c149e0286894280de7d72
KushalKothari-KK <kushalvkotharikvk@gmail.com>
2022-09-16 15:37:54 +0530

    From 12-15

--- ce-typescript/react-typescript-demo/src/App.tsx
+++ ce-typescript/react-typescript-demo/src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import Button from "./components/Button";
 import Container from "./components/Container";
+import { Box } from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { UserC } from "./components/context/UserC";
+import { UserContextProvider } from "./components/context/UserContext";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
 import Input from "./components/input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import { Counter } from "./components/state/Counter";
 import { LoggedIn } from "./components/state/LoggedIn";
 import { User } from "./components/state/User";
 import Status from "./components/Status";
+
 
 function App() {
   const personName = {
@@ -40,13 +46,22 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to abc</Heading>
       </Oscar>
-      <Button handleClick={(event,id)=>{
-        console.log("Handle click",event,id)
-      }}/>
-      <Input value='' handleChange={(event)=>console.log(event)}/>
-      <Container styles={{border:'1px solid #000',padding:'1rem'}}/>
-      <LoggedIn/>
-      <User/>
+      <Button
+        handleClick={(event, id) => {
+          console.log("Handle click", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid #000", padding: "1rem" }} />
+      <LoggedIn />
+      <User />
+      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <UserContextProvider>
+        <UserC/>
+      </UserContextProvider>
     </div>
   );
 }
